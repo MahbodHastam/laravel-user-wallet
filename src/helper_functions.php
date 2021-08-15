ac04a6e05a261d4b71304e517fb1160cc832e0a0
MahbodHastam <mahbod4782@gmail.com>
2021-08-16 01:09:13 +0430

    🤝 Added a helper function for get the wallet

--- src/helper_functions.php
+++ src/helper_functions.php
@@ -1,8 +1,38 @@
 <?php
 
-if (! function_exists('createHash')) {
+use Illuminate\Database\Eloquent\Model;
+use MahbodHastam\UserWallet\Models\UserWalletModel;
+
+if (!function_exists('createHash')) {
     function createHash(): string
     {
         return hash('sha256', uniqid((string) time()));
+    }
+}
+
+if (!function_exists('get_wallet')) {
+    function get_wallet(string | int | UserWalletModel | null $wallet_id_or_token): UserWalletModel | Model | null
+    {
+        if ($wallet_id_or_token instanceof UserWalletModel) {
+            return $wallet_id_or_token;
+        }
+
+        if (is_null($wallet_id_or_token)) {
+            return null;
+        }
+
+        if (in_array(gettype($wallet_id_or_token), ['string', 'integer'])) {
+            $wallet = UserWalletModel::query()->find($wallet_id_or_token);
+
+            if (!$wallet) {
+                $wallet = UserWalletModel::getByToken($wallet_id_or_token);
+            }
+        }
+
+        if (!$wallet) {
+            throw new \Error('Something went wrong: Wallet not found.');
+        }
+
+        return $wallet;
     }
 }
