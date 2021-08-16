63a3f9bf9fb54108a4106d4913d5e9dbfec45682
MahbodHastam <mahbod4782@gmail.com>
2021-08-16 19:11:55 +0430

    ✅ Added blade directives

--- src/helper_functions.php
+++ src/helper_functions.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
 use MahbodHastam\UserWallet\Models\UserWalletModel;
 
@@ -11,7 +12,7 @@ if (!function_exists('createHash')) {
 }
 
 if (!function_exists('get_wallet')) {
-    function get_wallet(string | int | UserWalletModel | null $wallet_id_or_token): UserWalletModel | Model | null
+    function get_wallet(string | int | UserWalletModel | null $wallet_id_or_token): UserWalletModel | Model | EloquentBuilder | null
     {
         if ($wallet_id_or_token instanceof UserWalletModel) {
             return $wallet_id_or_token;
